17d461d3da72ac47d1f08211341c643fa5e61391
A. Bram Neijt <bneijt@gmail.com>
2013-08-25 17:56:21 +0200

    Place images in matrix

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
+import Rendering
+import System.Console.CmdArgs
 
 
-main = putStrLn "HELLO"
+data CommitBook = CommitBook {
+        width :: Int,
+        height :: Int,
+        files :: [String]
+    } deriving (Show, Data, Typeable)
+
+options = CommitBook {
+        width = 3180,
+        height = 2382,
+        files = [] &= typFile &= args
+    }
+
+main = do
+    arguments <- cmdArgs options
+    renderImages (width arguments) (height arguments) (files arguments)
+
+
---
+++ src/Rendering.hs
@@ -0,0 +1,77 @@
+module Rendering(renderImages, renderPixbuf) where
+import Graphics.UI.Gtk (initGUI)
+import Graphics.Rendering.Cairo
+import Graphics.UI.Gtk.Gdk.Pixbuf (pixbufNewFromFile, pixbufGetWidth, pixbufGetHeight, Pixbuf (..))
+import Graphics.UI.Gtk.Cairo (setSourcePixbuf)
+import Control.Monad.IO.Class (MonadIO)
+data ImageMatrix = ImageMatrix {
+        matrixWidth :: Double,
+        matrixHeight :: Double,
+        imagesHorizontally :: Int,
+        imagesVertically :: Int
+    }
+
+
+
+scaleNeededFor :: ImageMatrix -> Double -> Double -> Double
+scaleNeededFor matrix imageWidth imageHeight = min widthScale heightScale
+    where
+        shouldBeWidth = (matrixWidth matrix) / (fromIntegral $ imagesHorizontally matrix)
+        shouldBeHeight = (matrixHeight matrix) / (fromIntegral $ imagesVertically matrix)
+        widthScale = shouldBeWidth / (imageWidth)
+        heightScale = shouldBeHeight / (imageHeight)
+
+renderPixbuf :: MonadIO m => Double -> Double -> Double -> Surface -> Pixbuf -> Double -> Double -> m ()
+renderPixbuf width height scaling surface pbImageA xOffset yOffset = renderWith surface $ do
+        -- rectangle xOffset yOffset width height
+        save
+        translate xOffset yOffset
+        scale scaling scaling
+        setSourcePixbuf pbImageA 0 0
+        paint
+        restore
+
+
+locationsInMatrix :: ImageMatrix -> [(Double, Double)]
+locationsInMatrix matrix = coords
+    where
+        widthPerImage = matrixWidth matrix / (fromIntegral $ imagesHorizontally matrix)
+        heightPerImage = matrixHeight matrix / (fromIntegral $ imagesVertically matrix)
+        coords = [(widthPerImage * (fromIntegral x), heightPerImage * (fromIntegral y)) | y <- [0..(imagesVertically matrix) -1], x <- [0..(imagesHorizontally matrix - 1)]]
+
+
+combineImages :: ImageMatrix -> [IO Pixbuf] -> Surface -> IO ()
+combineImages matrix images surface = do
+    firstImage <- head images
+    imageWidth <- fmap fromIntegral (pixbufGetWidth firstImage)
+    imageHeight <- fmap fromIntegral (pixbufGetHeight firstImage)
+
+    let scaling = scaleNeededFor matrix imageWidth imageHeight
+    renderWith surface $ do
+        setSourceRGB 1 1 1
+        rectangle 0 0 (matrixHeight matrix) (matrixHeight matrix)
+        paint
+    let place = renderPixbuf imageWidth imageHeight scaling surface :: Pixbuf -> Double -> Double -> IO()
+    pbImageA <- head images
+    let imagesAndLocations = zip (locationsInMatrix matrix) images
+    putStrLn ("Placing " ++ (show $ length imagesAndLocations))
+    mapM (\((x,y), ioImage) -> do
+            img <- ioImage
+            putStrLn ("Placing image at " ++ (show x) ++ " " ++ (show y))
+            place img x y
+            return ()
+        ) imagesAndLocations
+    --place pbImageA 600 60
+    surfaceWriteToPNG surface "z.png"
+
+renderImages :: Int -> Int -> [String] -> IO()
+renderImages width height imageFileNames = do
+    initGUI -- Initialize GDK for jpeg loading
+    withImageSurface FormatRGB24 width height (combineImages (ImageMatrix (fromIntegral width) (fromIntegral height) 3 4) (map pixbufNewFromFile imageFileNames))
+
+
+
+
+
+
+
